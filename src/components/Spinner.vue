1259a57456051fd7c8a91ee9cd026b068ef63dec
schnubor <info@chko.org>
2016-09-21 01:26:00 +0200

    rate limiting

---
+++ src/components/Spinner.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+    <div class="loader"></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    computed: {},
+    ready() {},
+    attached() {},
+    methods: {},
+    components: {}
+}
+</script>
+
+<style lang="css">
+.loader {
+  margin: 0px auto;
+  font-size: 10px;
+  position: relative;
+  border-top: 5px solid rgba(206,205,207, 0.2);
+  border-right: 5px solid rgba(206,205,207, 0.2);
+  border-bottom: 5px solid rgba(206,205,207, 0.2);
+  border-left: 5px solid #cecdcf;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+.loader,
+.loader:after {
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+}
+@-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/views/Vinyl.vue
+++ src/views/Vinyl.vue
@@ -8,9 +8,12 @@
             <hr>
         </nav>
 
-        <div class="row">
-            <div class="text-xs-center" v-if="!fetched">Digging ...</div>
-            <div class="col-md-3" v-if="fetched">
+        <div class="spinnerWrapper" v-if="!fetched">
+            <spinner></spinner>
+        </div>
+
+        <div class="row" v-if="fetched">
+            <div class="col-md-3">
                 <div class="card">
                     <img class="card-img-top" :src="vinylData.images[0].uri" alt="Card image cap" width="100%">
                     <div class="card-block">
@@ -21,6 +24,7 @@
                         Discogs
                     </div>
                 </div>
+                <button type="button" class="btn btn-outline-danger btn-lg btn-block" v-on:click="fetchVinylList">Nope, next!</button>
             </div>
             <div class="col-md-9">
                 <div class="videoWrapper">
@@ -34,6 +38,8 @@
 </template>
 
 <script>
+import Spinner from '../components/Spinner.vue'
+
 export default {
   data () {
     return {
@@ -42,7 +48,8 @@ export default {
         vinylData: {},
         vinylList: {
             page: 0
-        }
+        },
+        rateLimit: 10000
     }
   },
   computed: {
@@ -66,16 +73,20 @@ export default {
   attached () {},
   methods: {
       fetchVinylList() {
+          console.log("fetching new vinyl...");
+          this.fetched = false;
+
           let url = '';
 
           if(this.$route.params.genre) var page = Math.floor(Math.random() * (1000 - 1) + 1) + '&per_page=100';
-          if(this.$route.params.genre) var page = '1&per_page=100';
+          if(this.$route.params.artist) var page = '1&per_page=100';
 
           if(this.$route.params.artist) url = 'https://api.discogs.com/database/search?type=release&format=vinyl&artist=' + this.$route.params.artist + '&token=oXTNwXoHFojhFqXDzcLIfXjnzoiBkgTHYnIAiynU&page=' + page;
           if(this.$route.params.genre) url = 'https://api.discogs.com/database/search?type=release&format=vinyl&genre=' + this.$route.params.genre + '&token=oXTNwXoHFojhFqXDzcLIfXjnzoiBkgTHYnIAiynU&page=' + page;
           if(this.$route.params.random) url = 'https://api.discogs.com/database/search?type=release&format=vinyl&token=oXTNwXoHFojhFqXDzcLIfXjnzoiBkgTHYnIAiynU&page=' + page;
 
           var request = new XMLHttpRequest();
+
           request.open('GET', url, true);
 
           request.onload = () => {
@@ -95,12 +106,16 @@ export default {
           request.onerror = () => {
               console.log('god damn...');
               this.error = true;
+              setTimeout(() => {
+                  this.fetchVinylList();
+              }, this.rateLimit);
           };
 
           request.send();
       },
       fetchVinylData(uri) {
           var request = new XMLHttpRequest();
+
           request.open('GET', uri + '?token=oXTNwXoHFojhFqXDzcLIfXjnzoiBkgTHYnIAiynU', true);
 
           request.onload = () => {
@@ -109,13 +124,15 @@ export default {
                   this.error = false;
                   var resp = JSON.parse(request.responseText);
                   console.log(resp);
-                  if(resp.videos) {
+                  if(resp.videos && resp.images) {
                       this.vinylData = resp;
                       this.vinylData.videoUri = 'http://www.youtube.com/embed/' + resp.videos[0].uri.substr(resp.videos[0].uri.length - 11) + '?autoplay=1';
                       this.fetched = true;
                   }
                   else{
-                      this.fetchVinylList();
+                      setTimeout(() => {
+                          this.fetchVinylList();
+                      }, this.rateLimit);
                   }
               } else {
                   // We reached our target server, but it returned an error
@@ -126,6 +143,9 @@ export default {
 
           request.onerror = () => {
               console.log('god damn...');
+              setTimeout(() => {
+                  this.fetchVinylList();
+              }, this.rateLimit);
               this.error = true;
               this.fetched = false;
           };
@@ -135,11 +155,16 @@ export default {
 
 
   },
-  components: {}
+  components: {
+      spinner: Spinner
+  }
 }
 </script>
 
 <style lang="css">
+    .spinnerWrapper {
+        margin-top: 100px;
+    }
     .videoWrapper {
     	position: relative;
     	padding-bottom: 56.25%; /* 16:9 */
